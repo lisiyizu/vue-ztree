693313339f2500a8d9f62f6cb157e3e64538b346
lisiyizu <512235550@qq.com>
2016-11-01 12:55:53 +0800

    开源vue-ztree 树结构组件

---
+++ src/App.vue
@@ -0,0 +1,63 @@
+<template>
+  <div id="app">
+    <h1>{{ msg }}</h1>
+    <div style='width:280px;'>
+      <vue-ztree :list.sync='ztreeDataSource' :func.sync='nodeClick' :is-open='true'></vue-ztree>
+    </div>
+  </div>
+</template>
+
+<script>
+import vueZtree from './component/vue-ztree.vue'
+export default {
+  data () {
+    return {
+      msg: 'Hello Vue-Ztree!',
+      ztreeDataSource:[{
+                id:1,
+                name:"音乐",
+                children:[]
+      },{
+          id:2,
+          name:"视频",
+          children:[{
+             id:3,
+             name:"电影",
+             children:[{
+                id:4,
+                name:"国产电影"
+             },{
+                id:5,
+                name:"好莱坞电影"
+             },{
+                id:6,
+                name:"小语种电影"
+             }]
+          },{
+             id:7,
+             name:"短片",
+             children:[{
+                id:9,
+                name:"电视剧"
+             },{
+                id:10,
+                name:"短片"
+             }]
+          }]
+      }]
+    }
+  },
+  methods:{
+    nodeClick:function(m){
+       console.log(JSON.parse(JSON.stringify(m)));
+    }
+  },
+  components:{
+    vueZtree
+  }
+}
+</script>
+
+<style>
+body {font-family: Helvetica, sans-serif;}
+</style>
